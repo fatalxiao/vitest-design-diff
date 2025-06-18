1d0a459ad890f69a90346ab5d88928fa78613937
LiangXiaoJun <fatalxiao@163.com>
2025-06-18 13:43:18 +0800

    feat: add initial component and corresponding tests for design validation

---
+++ test/BigRectangle/Component.tsx
@@ -0,0 +1,16 @@
+// Types
+import type { FC } from 'react';
+
+const Component: FC = () => {
+    return (
+        <div
+            style={{
+                width: 3840,
+                height: 2160,
+                background: '#000',
+            }}
+        />
+    );
+};
+
+export default Component;
